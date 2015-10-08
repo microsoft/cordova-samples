6eb5d8c5a1b8638ec7c0bf9d40795d47b7725799
Chuck Lantz <clantz@microsoft.com>
2015-10-08 10:11:16 -0700

    Update taco_create.js

--- sap-custom-fiori-client/taco_create.js
+++ sap-custom-fiori-client/taco_create.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-
+/*
+  Copyright (c) Microsoft. All rights reserved.  
+  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+*/
 var fs = require('fs'),
     path = require('path'),
     os = require('os'),
