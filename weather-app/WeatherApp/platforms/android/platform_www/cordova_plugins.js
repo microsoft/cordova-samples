297c1b742efbcb8b9120bdd401c4570dc2159b7a
Norm Estabrook <normesta@microsoft.com>
2016-02-02 15:17:51 -0800

    Uploading updated weather app sample

---
+++ weather-app/WeatherApp/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,31 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-geolocation": "2.1.0"
+}
+// BOTTOM OF METADATA
+});
