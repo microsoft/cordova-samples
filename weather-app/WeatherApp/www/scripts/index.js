01828c8daded87cf288a0ffae7b8e82f14e9fdc2
Javier Rebagliatti <javier.rebagliatti@southworks.net>
2016-02-22 17:08:55 -0300

    Updated solution look & feel

--- weather-app/WeatherApp/www/scripts/index.js
+++ weather-app/WeatherApp/www/scripts/index.js
@@ -3,17 +3,19 @@
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
-    "use strict";
+    'use strict';
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind(this), false);
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
-        $('#get-weather').click(getWeather);
-        getLocation();
-        
+
+        $('#get-weather-btn').click(WeatherApp.getWeather);
+
+        WeatherApp.getLocation();
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
@@ -24,4 +26,4 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
