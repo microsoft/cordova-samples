2f0628255b45ceeb009a959e93870827c54b2086
Norm Estabrook <normesta@microsoft.com>
2016-03-25 13:01:43 -0700

    uploading geo sample

---
+++ cordova-plugin-geolocation/cordova-plugin-geolocation/www/scripts/customlibraries/map.js
@@ -0,0 +1,78 @@
+﻿
+var Latitude = undefined;
+var Longitude = undefined;
+
+// Get geo coordinates
+
+function getMapLocation() {
+
+    navigator.geolocation.getCurrentPosition(onMapSuccess, onMapError, { enableHighAccuracy: true });
+}
+
+// Success callback for get geo coordinates
+
+var onMapSuccess = function (position) {
+    
+    Latitude = position.coords.latitude;
+    Longitude = position.coords.longitude;
+
+    getMap(Latitude, Longitude);
+
+}
+
+// Get map by using coordinates
+
+function getMap(latitude, longitude) {
+
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 1,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+    var latLong = new google.maps.LatLng(latitude, longitude);
+
+    var marker = new google.maps.Marker({
+        position: latLong
+    });
+
+    marker.setMap(map);
+    map.setZoom(15);
+    map.setCenter(marker.getPosition());
+}
+
+// Success callback for watching your changing position
+
+var onMapWatchSuccess = function (position) {
+
+    var updatedLatitude = position.coords.latitude;
+    var updatedLongitude = position.coords.longitude;
+
+    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+
+        Latitude = updatedLatitude;
+        Longitude = updatedLongitude;
+
+        getMap(updatedLatitude, updatedLongitude);
+    }
+}
+
+// Error callback
+
+function onMapError(error) {
+    console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+// Watch your changing position
+
+function watchMapPosition() {
+
+    return navigator.geolocation.watchPosition(onMapWatchSuccess, onMapError, { enableHighAccuracy: true });  
+}
+
+
+
---
+++ cordova-plugin-geolocation/cordova-plugin-geolocation/www/scripts/index.js
@@ -0,0 +1,140 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+
+var watchID = undefined;
+
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        $('#check-div').hide();
+        $('#weather').hide();
+        $('#pictures').hide();
+        $('#places').hide();
+        $('#map').hide();
+
+        
+
+        $('#geo-select').on('change', function (e) {
+
+            if (this.value == "weather") {
+                $('#check-div').show();
+                $('#map').hide();
+                $('#pictures').hide();
+                $('#places').hide();
+                $('#weather').show();
+
+                getWeatherLocation();
+
+                $('check-div').attr('checked', false);
+
+                if (watchID != undefined) {
+                    navigator.geolocation.clearWatch(watchID);
+                }
+
+            } else if (this.value == "places") {
+
+                $('#check-div').show();
+                $('#weather').hide();
+                $('#pictures').hide();
+                $('#map').hide();
+                $('#places').show();
+
+                getPlacesLocation();
+
+                $('#geo_check').attr('checked', false);
+
+                if (watchID != undefined) {
+                    navigator.geolocation.clearWatch(watchID);
+                }
+
+            }else if (this.value == "pictures") {
+                $('#check-div').show();
+                $('#weather').hide();
+                $('#map').hide();
+                $('#places').hide();
+                $('#pictures').show();
+
+                getPicturesLocation();
+
+                $('#geo_check').attr('checked', false);
+
+                if (watchID != undefined) {
+                    navigator.geolocation.clearWatch(watchID);
+                }
+
+            } else if (this.value == "map") {
+                $('#check-div').show();
+                $('#weather').hide();
+                $('#pictures').hide();
+                $('#places').hide();
+                $('#map').show();
+
+                getMapLocation();
+
+                $('#geo_check').attr('checked', false);
+
+                if (watchID != undefined) {
+                    navigator.geolocation.clearWatch(watchID);
+                }
+
+            } else {
+                $('#check-div').hide();
+                $('#geo_check').attr('checked', false);
+
+                if (watchID != undefined) {
+                    navigator.geolocation.clearWatch(watchID);
+                }
+
+            }
+        });
+
+        $('#geo_check').on('change', function (e) {
+
+            if (this.checked == true) {
+
+                if ($('#weather').is(":visible")) {
+
+                    watchID = watchWeatherPosition();
+
+                } else if ($('#places').is(":visible")) {
+
+                    watchID = watchPlacesPosition();
+
+                } else if ($('#pictures').is(":visible")) {
+
+                    watchID = watchPicturePosition();
+
+                } else if ($('#map').is(":visible")) {
+
+                    watchID = watchMapPosition();
+                } else {
+
+                    // Do nothing.
+                }
+
+            } else {
+
+                navigator.geolocation.clearWatch(watchID);
+            }
+        });
+            
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+})();
+
